1280e8c08f920f4545c9660d003f9992876a2ebd
Tulsi Das <khatritulsidas@gmail.com>
2024-12-22 00:27:38 +0100

    add 2 primary colors

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Navigation } from "./navigation";
 export const Header = () => {
   return (
     <header className="flex flex-col gap-6">
-      <div className="bg-primary py-3">
+      <div className="bg-primary-1 py-3">
         <div className="container mx-auto px-4">
           <div className="flex justify-between">
             <p className="text-white">Pizzeria Roma Spandau</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        primary: "#3f2a15",
+        "primary-1": "#3f2a15",
+        "primary-2": "rgb(104, 89, 73)",
         "action-1": "#ac0000",
         "action-2": "#800101",
       },
