08da7ae97dc00f021dc63d22dd497d26219333f5
Tulsi Das <khatritulsidas@gmail.com>
2024-12-22 00:27:47 +0100

    add active link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,33 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-export const Navigation = () => (
-  <nav className="bg-primary container px-4 py-2 text-white">
-    <ul className="flex list-none gap-4">
-      <NavItem href="/">Welcome!</NavItem>
-      <NavItem href="/speisekarte">Food & Drink</NavItem>
-    </ul>
-  </nav>
-);
+export const Navigation = () => {
+  return (
+    <nav className="bg-primary-1 container px-4 text-white">
+      <ul className="flex list-none gap-4">
+        <NavItem href="/">Welcome!</NavItem>
+        <NavItem href="/speisekarte">Food & Drink</NavItem>
+      </ul>
+    </nav>
+  );
+};
 
-const NavItem = ({ children, href }) => (
-  <li>
-    <Link href={href}>{children}</Link>
-  </li>
-);
+const NavItem = ({ children, href }) => {
+  const pathname = usePathname();
+  const isActivePage = href === pathname;
+  return (
+    <li>
+      <Link
+        href={href}
+        className={
+          "block py-2" + (isActivePage ? " font-bold" : " hover:text-gray-200")
+        }
+      >
+        {children}
+      </Link>
+
+      {isActivePage ? <hr className="border-primary-2 border-b-2" /> : null}
+    </li>
+  );
+};
