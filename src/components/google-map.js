a8caba5f5758f0f41967606153cc75835fcb386f
Tulsi Das <khatritulsidas@gmail.com>
2024-12-22 00:26:43 +0100

    remove allow fullscreen

--- src/components/google-map.js
+++ src/components/google-map.js
@@ -4,7 +4,6 @@ const GoogleMap = () => (
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d151.67680559102308!2d13.204595!3d52.53653!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a857d95b6821e9%3A0xd242266478ff68f3!2sFlorida%20Eis%20Altstadt%20Spandau!5e0!3m2!1sen!2sus!4v1734819048513!5m2!1sen!2sus"
     style={{ border: 0, width: "100%", height: "400px" }}
-    allowFullScreen=""
     loading="lazy"
     referrerPolicy="no-referrer-when-downgrade"
   ></iframe>
