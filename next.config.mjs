206070efde62b7856ee5a2f684dc3ac88fc901c6
Tulsi Das <khatritulsidas@gmail.com>
2024-12-22 11:56:25 +0100

    remove i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  i18n: {
-    locales: ["en", "de"],
-    defaultLocale: "en",
-  },
 };
 
 export default nextConfig;
