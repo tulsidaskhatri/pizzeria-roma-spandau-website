0d68f02d18f4bf85670702b0291ba757d6cdecbf
Tulsi Das <khatritulsidas@gmail.com>
2024-12-22 20:52:45 +0100

    add content in home url

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,18 +1,6 @@
-import { Contact } from "@/components/contact";
-import { Credit } from "@/components/credit";
-import GoogleMap from "@/components/google-map";
-import { Hero } from "@/components/hero";
-import { OpeningTime } from "@/components/opening-time";
+import { HomeContent } from "@/components/home-content";
 
 export default async function Home({ params }) {
   const { locale } = await params;
-  return (
-    <main className="container bg-white">
-      <Hero locale={locale} />
-      <OpeningTime locale={locale} />
-      <GoogleMap />
-      <Contact locale={locale} />
-      <Credit />
-    </main>
-  );
+  return <HomeContent locale={locale} />;
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,31 +5,37 @@ import { usePathname } from "next/navigation";
 
 export const Navigation = ({ locale }) => {
   const t = tranlations[locale];
+  const pathname = usePathname();
   return (
     <nav className="container bg-primary-1 px-6 text-white sm:px-12">
       <ul className="flex list-none gap-4">
-        <NavItem href="/">{t.nav.links.welcome}</NavItem>
-        <NavItem href="/speisekarte">{t.nav.links.menu}</NavItem>
+        <NavItem href={`/${locale}`} active={pathname === "/" || `/${locale}`}>
+          {t.nav.links.welcome}
+        </NavItem>
+        <NavItem
+          href={`/${locale}/speisekarte`}
+          active={pathname === `/${locale}/speisekarte`}
+        >
+          {t.nav.links.menu}
+        </NavItem>
       </ul>
     </nav>
   );
 };
 
-const NavItem = ({ children, href }) => {
-  const pathname = usePathname();
-  const isActivePage = href === pathname;
+const NavItem = ({ children, href, active }) => {
   return (
     <li>
       <Link
         href={href}
         className={
-          "block py-2" + (isActivePage ? " font-bold" : " hover:text-gray-200")
+          "block py-2" + (active ? " font-bold" : " hover:text-gray-200")
         }
       >
         {children}
       </Link>
 
-      {isActivePage ? <hr className="border-b-2 border-primary-2" /> : null}
+      {active ? <hr className="border-b-2 border-primary-2" /> : null}
     </li>
   );
 };
