89031a091eef2f6bc07ef208c127d02cb4d55aec
Tulsi Das <khatritulsidas@gmail.com>
2024-12-23 17:48:39 +0100

    add menu title, subtitle and marketing text

--- src/app/[locale]/speisekarte/page.js
+++ src/app/[locale]/speisekarte/page.js
@@ -12,7 +12,16 @@ const Spiesen = ({ params }) => {
         className="w-full"
       />
 
-      <h1>{t.menuPage.title}</h1>
+      <div className="flex flex-col gap-5 py-4">
+        <div>
+          <h2 className="text-3xl text-action-2">{t.menuPage.title}</h2>
+          <p className="font-bold text-gray-900">{t.menuPage.subtitle}</p>
+        </div>
+        <div>
+          <p className="font-bold text-green-600">{t.menuPage.marketing1}</p>
+          <p className="text-xs text-green-600">{t.menuPage.marketing2}</p>
+        </div>
+      </div>
     </main>
   );
 };
