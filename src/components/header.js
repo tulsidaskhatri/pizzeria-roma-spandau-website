1d245e9879d249946168aacb4277ca5953635e40
Tulsi Das <khatritulsidas@gmail.com>
2024-12-25 00:09:41 +0100

    make header fixed

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import { Navigation } from "./navigation";
 export const Header = ({ locale }) => {
   return (
     <header className="flex flex-col gap-6">
-      <div className="bg-primary-1 py-3">
+      <div className="fixed w-full bg-primary-1 py-3">
         <div className="container mx-auto px-4">
           <p className="text-center font-bold text-white sm:text-left">
             Pizzeria Roma Spandau
