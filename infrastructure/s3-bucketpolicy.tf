0500a4c8eb7adada37ac17a6909c4c782dd05946
Tulsi Das <khatritulsidas@gmail.com>
2024-12-26 00:56:57 +0100

    cleanup

--- infrastructure/s3-bucketpolicy.tf
+++ infrastructure/s3-bucketpolicy.tf
@@ -1,28 +1,10 @@
-
-resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
-  bucket = data.aws_s3_bucket.selected-bucket.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-  depends_on = [aws_s3_bucket_public_access_block.example]
-}
-
-resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = data.aws_s3_bucket.selected-bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
 resource "aws_s3_bucket_policy" "bucket-policy" {
   bucket = data.aws_s3_bucket.selected-bucket.id
   policy = data.aws_iam_policy_document.iam-policy-1.json
 }
 data "aws_iam_policy_document" "iam-policy-1" {
   statement {
-    sid    = "AllowPublicRead"
+    sid    = "AllowCloudFrontRead"
     effect = "Allow"
     resources = [
       "arn:aws:s3:::www.${var.bucket_name}",
@@ -30,10 +12,9 @@ data "aws_iam_policy_document" "iam-policy-1" {
     ]
     actions = ["S3:GetObject"]
     principals {
-      type        = "*"
-      identifiers = ["*"]
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
     }
   }
-
-  depends_on = [aws_s3_bucket_public_access_block.example]
 }
+
--- infrastructure/s3acl.tf
+++ infrastructure/s3acl.tf
@@ -1,5 +1,4 @@
 resource "aws_s3_bucket_acl" "bucket-acl" {
-  bucket     = data.aws_s3_bucket.selected-bucket.id
-  acl        = "public-read"
-  depends_on = [aws_s3_bucket_ownership_controls.s3_bucket_acl_ownership]
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  acl    = "private"
 }
