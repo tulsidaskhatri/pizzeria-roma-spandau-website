b3fa9bd2c9541f824226b1ccbc7455fb8c261aa8
Tulsi Das <khatritulsidas@gmail.com>
2024-12-26 02:27:59 +0100

    make the bucket public

--- infrastructure/s3acl.tf
+++ infrastructure/s3acl.tf
@@ -1,4 +1,25 @@
-resource "aws_s3_bucket_acl" "bucket-acl" {
+resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = data.aws_s3_bucket.selected-bucket.id
-  acl    = "private"
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = data.aws_s3_bucket.selected-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = data.aws_s3_bucket.selected-bucket.id
+  acl    = "public-read"
 }
